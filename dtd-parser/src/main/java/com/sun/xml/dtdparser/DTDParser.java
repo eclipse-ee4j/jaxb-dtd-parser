ba01c5ede486b398a5f532dd1f19f05af90c2f91
Lukas Jungmann <lukas.jungmann@oracle.com>
2021-07-19 18:41:26 +0200

    typos
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- dtd-parser/src/main/java/com/sun/xml/dtdparser/DTDParser.java
+++ dtd-parser/src/main/java/com/sun/xml/dtdparser/DTDParser.java
@@ -23,6 +23,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
